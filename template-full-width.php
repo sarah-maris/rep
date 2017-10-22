63c92777f4338b8a5e508964982ceeb91376abe0
Sarah Maris <sarah@sarahmaris.com>
2017-10-21 22:35:51 -0400

    Initial commit

---
+++ template-full-width.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Template Name: Full Width
+ * Description: Full with page for RealEstatePeople.us
+ * Revised by Sarah Maris for RealEstatePeople.us
+ * Updated: 10-21-17
+ */
+
+get_header(); ?>
+
+<div class="wrap">
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+			<?php
+			while ( have_posts() ) : the_post();
+
+				get_template_part( 'template-parts/page/content', 'page' );
+
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
+
+			endwhile; // End of the loop.
+			?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+</div><!-- .wrap -->
+
+<?php get_footer();
---
+++ template-parts/footer/site-info.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Displays footer site info
+ * Revised by Sarah Maris for RealEstatePeople.us
+ * Updated: 10-21-17
+ */
+
+?>
+<div class="site-info">
+&copy; <?php $the_year = date("Y"); echo $the_year; ?> Real Estate People
+</div><!-- .site-info -->
